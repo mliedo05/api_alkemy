6fe5e3e16d4d574d393ca820be40dd25ac7f3a2c
mliedo05 <mliendo05@gmail.com>
2022-04-20 00:12:25 -0400

    try to done sendrid

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-    default :from => 'any_from_address@example.com'
+    default :from => 'mliendo05@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
     def send_signup_email(user)
